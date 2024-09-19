63725169e01afc8145b7a8ef773628c2aa87136f
tpcm <peadermartin@gmail.com>
2024-09-19 18:35:31 +0000

    feat: add a Point and Line class, update Window class to draw a line

--- src/graphics.py
+++ src/graphics.py
@@ -1,5 +1,20 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1: Point, p2: Point) -> None:
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas: Canvas, fill_colour: str):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_colour, width=2
+        )
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -21,3 +36,6 @@ class Window:
     def close(self):
         self.window_running = False
 
+    def draw_line(self, line: Line, fill_colour: str):
+        line.draw(self.__canvas, fill_colour)
+        
--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from .graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    
+    p1 = Point(5,5)
+    p2 = Point(250,250)
+    line = Line(p1, p2)
+    win.draw_line(line, fill_colour="black")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
