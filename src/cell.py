11b01b9ca7ddf3d2ad7962ae4d0224988bcff8d1
tpcm <peadermartin@gmail.com>
2024-09-21 08:03:10 +0000

    feat: add draw_move fn to draw lines between cells

--- src/cell.py
+++ src/cell.py
@@ -2,30 +2,48 @@ from graphics import Line, Point
 
 class Cell:
     def __init__(self, window) -> None:
-        self.__window = window
-        self.__x1 = None
-        self.__y1 = None
-        self.__x2 = None
-        self.__y2 = None
+        self._window = window
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
 
     def draw(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
             left_line = Line(Point(x1, y1), Point(x1, y2))
-            self.__window.draw_line(left_line, fill_colour="black")
+            self._window.draw_line(left_line, fill_colour="black")
         if self.has_right_wall:
             right_line = Line(Point(x2, y1), Point(x2, y2))
-            self.__window.draw_line(right_line, fill_colour="black")
+            self._window.draw_line(right_line, fill_colour="black")
         if self.has_top_wall:
             top_line = Line(Point(x1, y1), Point(x2, y1))
-            self.__window.draw_line(top_line, fill_colour="black")
+            self._window.draw_line(top_line, fill_colour="black")
         if self.has_bottom_wall:
             bottom_line = Line(Point(x1, y2), Point(x2, y2))
-            self.__window.draw_line(bottom_line, fill_colour="black")
+            self._window.draw_line(bottom_line, fill_colour="black")
+    
+    def draw_move(self, to_cell, undo=False):
+        x_midpoint_1 = self._x1 + 0.5*(self._x2 - self._x1)
+        y_midpoint_1 = self._y1 - 0.5*(self._y1 - self._y2)
+
+        x_midpoint_2 = to_cell._x1 + 0.5*(to_cell._x2 - to_cell._x1)
+        y_midpoint_2 = to_cell._y1 - 0.5*(to_cell._y1 - to_cell._y2)
+
+        line = Line(
+            Point(x_midpoint_1, y_midpoint_1),
+            Point(x_midpoint_2, y_midpoint_2)
+        )
+        if undo:
+            fill_colour = "grey"
+        else:
+            fill_colour = "red"
+
+        self._window.draw_line(line, fill_colour)
--- src/main.py
+++ src/main.py
@@ -9,38 +9,43 @@ def main():
     line = Line(p1, p2)
     # win.draw_line(line, fill_colour="black")
     
-    cell = Cell(window=win)
-    cell.has_bottom_wall = False
-    cell.draw(x1=5, y1=5, x2=50, y2=50)
+    cell_1 = Cell(window=win)
+    cell_1.has_bottom_wall = False
+    cell_1.draw(x1=5, y1=5, x2=50, y2=50)
 
-    cell = Cell(window=win)
-    cell.has_top_wall = False
-    cell.has_bottom_wall = False
-    cell.has_right_wall = False
-    cell.draw(x1=5, y1=50, x2=50, y2=100)
+    cell_2 = Cell(window=win)
+    cell_2.has_top_wall = False
+    cell_2.has_bottom_wall = False
+    cell_2.has_right_wall = False
+    cell_2.draw(x1=5, y1=50, x2=50, y2=100)
 
-    cell = Cell(window=win)
-    cell.has_top_wall=False
-    cell.draw(x1=5, y1=100, x2=50, y2=150)
+    cell_3 = Cell(window=win)
+    cell_3.has_top_wall=False
+    cell_3.draw(x1=5, y1=100, x2=50, y2=150)
 
-    cell = Cell(window=win)
-    cell.has_left_wall=False
-    cell.has_right_wall = False
-    cell.draw(x1=50, y1=50, x2=100, y2=100)
+    cell_4 = Cell(window=win)
+    cell_4.has_left_wall=False
+    cell_4.has_right_wall = False
+    cell_4.draw(x1=50, y1=50, x2=100, y2=100)
 
-    cell = Cell(window=win)
-    cell.has_bottom_wall = False
-    cell.draw(x1=100, y1=5, x2=145, y2=50)
+    cell_5 = Cell(window=win)
+    cell_5.has_bottom_wall = False
+    cell_5.draw(x1=100, y1=5, x2=145, y2=50)
 
-    cell = Cell(window=win)
-    cell.has_top_wall = False
-    cell.has_bottom_wall = False
-    cell.has_left_wall = False
-    cell.draw(x1=100, y1=50, x2=145, y2=100)
+    cell_6 = Cell(window=win)
+    cell_6.has_top_wall = False
+    cell_6.has_bottom_wall = False
+    cell_6.has_left_wall = False
+    cell_6.draw(x1=100, y1=50, x2=145, y2=100)
 
-    cell = Cell(window=win)
-    cell.has_top_wall=False
-    cell.draw(x1=100, y1=100, x2=145, y2=150)
+    cell_7 = Cell(window=win)
+    cell_7.has_top_wall=False
+    cell_7.draw(x1=100, y1=100, x2=145, y2=150)
+
+    cell_1.draw_move(cell_2, True)
+    cell_2.draw_move(cell_4, True)
+    cell_4.draw_move(cell_6, True)
+    cell_6.draw_move(cell_7)
 
     
     win.wait_for_close()
